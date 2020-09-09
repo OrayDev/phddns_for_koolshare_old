954bbb793006da878153621e89c0af7f1ed0f356
liuqiang <liuqiang@oray.com>
2020-09-09 16:01:29 +0800

    initial commit

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "花生壳内网版3.0 （phddns） 动态域名 内网穿透", 
+    "home_url": "Module_phddns.asp", 
+    "md5": "b4ad75ee35832ddac71be34ddab5e927", 
+    "module": "phddns", 
+    "title": "3.0", 
+    "version": "3.0.2"
+}
