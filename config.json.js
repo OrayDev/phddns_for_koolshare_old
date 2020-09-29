e97f7468fa67e8f674f5fc52f0c5915c3662bcbc
liuqiang <liuqiang@oray.com>
2020-09-29 15:30:32 +0800

    开机自启动

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "花生壳内网版3.0 （phddns） 动态域名 内网穿透", 
     "home_url": "Module_phddns.asp", 
-    "md5": "b4ad75ee35832ddac71be34ddab5e927", 
+    "md5": "592459e7e72a71919d7ad489e0d3904e", 
     "module": "phddns", 
     "title": "花生壳3.0", 
     "version": "3.0.2"
